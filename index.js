3c281676afd5f257ce47f42e7822da7b0bdcb049
DiffalexI <ahmedsamh12ha@gmail.com>
2024-11-08 22:37:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-module.exports = (req, res) => {
-    res.json({ message: "Hello, world!" });
-};
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from Express!' });
+});
+
+module.exports = app;
